9028571433403809ccc66eb6bad3f20c41a711e6
pedro <joaopsj007@gmail.com>
2026-02-02 16:38:39 -0300

    chore: backend/ implement database config and TypeOrm

--- HelpDesk_backend/src/app.module.ts
+++ HelpDesk_backend/src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        entities: [__dirname + '/entities/*.ts'],
+        migrations: [__dirname + '/database/migrations/*.ts'],
+        autoLoadEntities: true,
+        synchronize: false,
+        logging: false,
+      }),
     }),
   ],
   controllers: [AppController],
