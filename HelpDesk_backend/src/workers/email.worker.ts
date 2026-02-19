08377c447e906acd734155d37bfa9e94eaff00b1
pedro <joaopsj007@gmail.com>
2026-02-19 18:48:50 -0300

    test: backend/ implement unit tests for email.worker

--- HelpDesk_backend/src/workers/email.worker.ts
+++ HelpDesk_backend/src/workers/email.worker.ts
@@ -6,22 +6,28 @@ import { bullmqConnectionOptions } from '../config/redis.connection';
 import { jobs } from '../app/common/rules/jobs';
 import { SendEmailDTO } from 'src/app/modules/email/dtos/send-email.dto';
 
+export async function processEmailJob(
+  emailService: EmailService,
+  job: Job<SendEmailDTO, any, string>,
+): Promise<void> {
+  switch (job.name) {
+    case jobs.send_email.name:
+      await emailService.send(job.data);
+      break;
+
+    default:
+      throw new Error(`Unknown job name: ${job.name}`);
+  }
+}
+
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(WorkerModule);
 
   const emailService = app.get(EmailService);
 
-  new Worker(
-    'email-queue',
-    async (job: Job<SendEmailDTO, any, string>) => {
-      if (job.name === jobs.send_email.name) {
-        await emailService.send(job.data);
-      }
-    },
-    {
-      connection: bullmqConnectionOptions,
-    },
-  );
+  new Worker('email-queue', (job) => processEmailJob(emailService, job), {
+    connection: bullmqConnectionOptions,
+  });
 }
 
 bootstrap();
