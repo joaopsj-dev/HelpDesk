ce00d7870f883a3cc6cb479ad0eb7a6bf8285cfb
pedro <joaopsj007@gmail.com>
2026-02-19 15:42:09 -0300

    refactor: backend/ move email queue to email module

--- HelpDesk_backend/src/queue/email.queue.ts
+++
@@ -1,6 +0,0 @@
-import { Queue } from 'bullmq';
-import { bullmqConnectionOptions } from './redis.connection';
-
-export const emailQueue = new Queue('email-queue', {
-  connection: bullmqConnectionOptions,
-});
